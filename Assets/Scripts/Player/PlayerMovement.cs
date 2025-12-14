4c6da93dba1604056bc28d93ede5ad3d7a3e056d
praseth002 <prasethsokha@gmail.com>
2025-12-14 09:06:59 +0700

    2 player controls

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,228 +1,3 @@
-// using UnityEngine;
-// using UnityEngine.InputSystem;
-
-// public class PlayerMovement : MonoBehaviour
-// {
-//     [Header("Movement Settings")]
-//     public float moveSpeed = 5f;
-//     public float jumpForce = 12f;
-
-//     [Header("Dash Settings")]
-//     public float dashSpeed = 15f;
-//     public float dashDuration = 0.2f;
-//     public float doubleTapTime = 0.25f;
-
-//     private Rigidbody2D rb;
-//     private PlayerControls controls;
-
-//     private bool isGrounded;
-//     private bool isDashing = false;
-//     private float dashTimeLeft = 0f;
-
-//     private float lastTapTime = 0f;
-//     private float lastDirection = 0f;
-
-//     [SerializeField] private Animator animator;
-
-//     private bool isAttacking = false;
-//     private float attackTime;
-
-//     [Header("Attack Durations")]
-//     public float punchDuration = 0.25f;
-//     public float kickDuration = 0.40f;
-//     public float specialDuration = 0.50f;
-
-//     public GameObject punchHitbox;
-//     public GameObject kickHitbox;
-//     // public GameObject specialHitbox;
-
-//     public void EnablePunchHitbox()
-//     {
-//         punchHitbox.SetActive(true);
-//         Debug.Log("Punch Hitbox ENABLED");
-//     }
-
-//     public void EnableKickHitbox()
-//     {
-//         kickHitbox.SetActive(true);
-//         Debug.Log("Kick Hitbox ENABLED");
-//     }
-
-//     // public void EnableSpecialHitbox()
-//     // {
-//     //     specialHitbox.SetActive(true);
-//     //     Debug.Log("Special Hitbox ENABLED");
-//     // }
-
-//     public void DisableHitboxes()
-//     {
-//         punchHitbox.SetActive(false);
-//         kickHitbox.SetActive(false);
-//         // specialHitbox.SetActive(false);
-
-//         Debug.Log("All Hitboxes DISABLED");
-//     }
-
-
-
-
-//     private void Awake()
-//     {
-//         rb = GetComponent<Rigidbody2D>();
-//         controls = new PlayerControls();
-
-//         // Jump input
-//         controls.Player.Jump.performed += ctx => Jump();
-
-//         // Move input (for double-tap dash detection)
-//         controls.Player.Move.performed += ctx => OnMoveInput(ctx.ReadValue<float>());
-
-//         controls.Player.Punch.performed += ctx => Punch();
-//         controls.Player.Kick.performed += ctx => Kick();
-//         controls.Player.Special.performed += ctx => Special();
-
-//     }
-
-//     private void OnEnable() => controls.Enable();
-//     private void OnDisable() => controls.Disable();
-
-//     private void Update()
-//     {
-//         // If attacking, freeze movement
-//         if (isAttacking)
-//         {
-//             animator.SetBool("IsRunning", false);
-
-//             attackTime -= Time.deltaTime;
-
-//             if (attackTime <= 0f)
-//                 isAttacking = false;
-
-//             return;
-//         }
-
-//         float move = controls.Player.Move.ReadValue<float>();
-
-//         // Dash logic
-//         if (isDashing)
-//         {
-//             dashTimeLeft -= Time.deltaTime;
-
-//             if (dashTimeLeft <= 0f)
-//                 isDashing = false;
-
-//             // Lock horizontal velocity while dashing
-//             rb.velocity = new Vector2(lastDirection * dashSpeed, rb.velocity.y);
-
-//             // Skip normal movement/animation while dashing
-//             animator.SetBool("IsRunning", false);
-//             return;
-//         }
-
-//         // Normal horizontal movement
-//         if (isGrounded)
-//         {
-//             rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
-
-//             // Running animation
-//             animator.SetBool("IsRunning", move != 0);
-//         }
-//         else
-//         {
-//             // In air: no running animation
-//             animator.SetBool("IsRunning", false);
-//         }
-//     }
-
-
-//     private void OnMoveInput(float move)
-//     {
-//         if (move != 0 && isGrounded) // Only allow dash if grounded
-//         {
-//             // Double-tap detection
-//             if (move == lastDirection && (Time.time - lastTapTime) < doubleTapTime)
-//             {
-//                 StartDash(move);
-//             }
-//             else
-//             {
-//                 lastDirection = move;
-//                 lastTapTime = Time.time;
-//             }
-//         }
-//     }
-
-//     private void Jump()
-//     {
-//         animator.SetBool("IsRunning", false);
-//         if (!isGrounded) return;
-
-//         float move = controls.Player.Move.ReadValue<float>();
-//         float jumpX = move * moveSpeed;
-
-//         rb.velocity = new Vector2(jumpX, jumpForce);
-
-//         animator.SetBool("IsJumping", true);
-//         animator.Play("PlayerJump", 0, 0f);
-//         isGrounded = false;
-//     }
-
-//     private void OnCollisionEnter2D(Collision2D collision)
-//     {
-//         if (collision.collider.CompareTag("Ground"))
-//         {
-//             isGrounded = true;
-//             animator.SetBool("IsJumping", false);
-//         }
-//     }
-
-//     private void StartDash(float direction)
-//     {
-//         isDashing = true;
-//         dashTimeLeft = dashDuration;
-//         lastDirection = direction;
-
-//         // Apply dash velocity instantly
-//         rb.velocity = new Vector2(direction * dashSpeed, rb.velocity.y);
-
-//         // Optional: dash animation
-//         // animator.Play("Dash", -1, 0f);
-//     }
-
-//     private void Punch()
-//     {
-//         if (isAttacking || !isGrounded) return;
-
-//         isAttacking = true;
-//         attackTime = punchDuration;
-
-//         animator.Play("PlayerPunch", 0, 0f);
-//     }
-
-//     private void Kick()
-//     {
-//         if (isAttacking || !isGrounded) return;
-
-//         isAttacking = true;
-//         attackTime = kickDuration;
-
-//         animator.Play("PlayerKick", 0, 0f);
-//     }
-
-//     private void Special()
-//     {
-//         if (isAttacking || !isGrounded) return;
-
-//         isAttacking = true;
-//         attackTime = specialDuration;
-
-//         animator.Play("PlayerSpecial", 0, 0f);
-//     }
-
-
-// }
-
-
 using UnityEngine;
 
 public enum PlayerState
@@ -237,6 +12,10 @@ public enum PlayerState
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Player")]
+    public bool isPlayer2 = false;
+    public Transform opponent;
+
     [Header("Movement Settings")]
     public float moveSpeed = 5f;
     public float jumpForce = 12f;
@@ -246,21 +25,6 @@ public class PlayerMovement : MonoBehaviour
     public float dashDuration = 0.2f;
     public float doubleTapTime = 0.25f;
 
-    private Rigidbody2D rb;
-    private PlayerControls controls;
-    private PlayerState currentState = PlayerState.Idle;
-
-    private bool isGrounded;
-    private bool isDashing = false;
-    private float dashTimeLeft = 0f;
-    private float lastTapTime = 0f;
-    private float lastDirection = 0f;
-
-    [SerializeField] private Animator animator;
-
-    private bool isAttacking = false;
-    private float attackTime;
-
     [Header("Attack Durations")]
     public float punchDuration = 0.25f;
     public float kickDuration = 0.40f;
@@ -268,59 +32,107 @@ public class PlayerMovement : MonoBehaviour
 
     public GameObject punchHitbox;
     public GameObject kickHitbox;
-    // public GameObject specialHitbox;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    // Input
+    private PlayerControls controlsP1;
+    private PlayerControls1 controlsP2;
+
+    private PlayerState currentState = PlayerState.Idle;
+
+    private bool isGrounded;
+    private bool isAttacking;
+    private float attackTime;
+
+    private bool isDashing;
+    private float dashTimeLeft;
+    private float lastTapTime;
+    private float lastDirection;
+    private SpriteRenderer spriteRenderer;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        controls = new PlayerControls();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 
-        controls.Player.Jump.performed += ctx => Jump();
-        controls.Player.Move.performed += ctx => OnMoveInput(ctx.ReadValue<float>());
-        controls.Player.Punch.performed += ctx => Punch();
-        controls.Player.Kick.performed += ctx => Kick();
-        // controls.Player.Special.performed += ctx => Special();
+        if (!isPlayer2)
+        {
+            controlsP1 = new PlayerControls();
+
+            controlsP1.Player.Jump.performed += _ => Jump();
+            controlsP1.Player.Move.performed += ctx => OnMoveInput(ctx.ReadValue<float>());
+            controlsP1.Player.Punch.performed += _ => Punch();
+            controlsP1.Player.Kick.performed += _ => Kick();
+            controlsP1.Player.Special.performed += _ => Special();
+        }
+        else
+        {
+            controlsP2 = new PlayerControls1();
+
+            controlsP2.Player.Jump.performed += _ => Jump();
+            controlsP2.Player.Move.performed += ctx => OnMoveInput(ctx.ReadValue<float>());
+            controlsP2.Player.Punch.performed += _ => Punch();
+            controlsP2.Player.Kick.performed += _ => Kick();
+            controlsP2.Player.Special.performed += _ => Special();
+        }
     }
 
-    private void OnEnable() => controls.Enable();
-    private void OnDisable() => controls.Disable();
+
+    private void OnEnable()
+    {
+        if (!isPlayer2) controlsP1.Enable();
+        else controlsP2.Enable();
+    }
+
+    private void OnDisable()
+    {
+        if (!isPlayer2) controlsP1.Disable();
+        else controlsP2.Disable();
+    }
 
     private void Update()
     {
-        if (currentState == PlayerState.Hit || currentState == PlayerState.Dead) return;
+        if (currentState == PlayerState.Hit || currentState == PlayerState.Dead)
+            return;
 
-        // Handle attacking freeze
+        FaceOpponent();
+
         if (isAttacking)
         {
             animator.SetBool("IsRunning", false);
             attackTime -= Time.deltaTime;
+
             if (attackTime <= 0f)
             {
                 isAttacking = false;
+                DisableHitboxes();
                 currentState = PlayerState.Idle;
             }
             return;
         }
 
-        float move = controls.Player.Move.ReadValue<float>();
+        float move = GetMoveInput();
 
-        // Dash
+        // Dash handling
         if (isDashing)
         {
             dashTimeLeft -= Time.deltaTime;
-            if (dashTimeLeft <= 0f) isDashing = false;
-
             rb.velocity = new Vector2(lastDirection * dashSpeed, rb.velocity.y);
-            animator.SetBool("IsRunning", false);
+
+            if (dashTimeLeft <= 0f)
+                isDashing = false;
+
             return;
         }
 
-        // Normal horizontal movement
         if (isGrounded)
         {
             rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
             animator.SetBool("IsRunning", move != 0);
-
             currentState = move != 0 ? PlayerState.Moving : PlayerState.Idle;
         }
         else
@@ -330,19 +142,25 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private float GetMoveInput()
+    {
+        return !isPlayer2
+       ? controlsP1.Player.Move.ReadValue<float>()
+       : controlsP2.Player.Move.ReadValue<float>();
+    }
+
     private void OnMoveInput(float move)
     {
-        if (move != 0 && isGrounded)
+        if (!isGrounded || move == 0) return;
+
+        if (move == lastDirection && Time.time - lastTapTime < doubleTapTime)
         {
-            if (move == lastDirection && (Time.time - lastTapTime) < doubleTapTime)
-            {
-                StartDash(move);
-            }
-            else
-            {
-                lastDirection = move;
-                lastTapTime = Time.time;
-            }
+            StartDash(move);
+        }
+        else
+        {
+            lastDirection = move;
+            lastTapTime = Time.time;
         }
     }
 
@@ -350,41 +168,24 @@ public class PlayerMovement : MonoBehaviour
     {
         if (!isGrounded) return;
 
-        float move = controls.Player.Move.ReadValue<float>();
-        float jumpX = move * moveSpeed;
+        float move = GetMoveInput();
+        rb.velocity = new Vector2(move * moveSpeed, jumpForce);
 
-        rb.velocity = new Vector2(jumpX, jumpForce);
         animator.SetBool("IsJumping", true);
         animator.Play("PlayerJump", 0, 0f);
+
         isGrounded = false;
         currentState = PlayerState.Jumping;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider.CompareTag("Ground"))
-        {
-            isGrounded = true;
-            animator.SetBool("IsJumping", false);
-            if (!isAttacking && currentState != PlayerState.Hit && currentState != PlayerState.Dead)
-                currentState = PlayerState.Idle;
-        }
-    }
-
-    private void StartDash(float direction)
-    {
-        isDashing = true;
-        dashTimeLeft = dashDuration;
-        lastDirection = direction;
-        rb.velocity = new Vector2(direction * dashSpeed, rb.velocity.y);
     }
 
     private void Punch()
     {
         if (isAttacking || !isGrounded) return;
+
         isAttacking = true;
         attackTime = punchDuration;
         currentState = PlayerState.Attacking;
+
         animator.Play("PlayerPunch", 0, 0f);
         punchHitbox.SetActive(true);
     }
@@ -392,33 +193,54 @@ public class PlayerMovement : MonoBehaviour
     private void Kick()
     {
         if (isAttacking || !isGrounded) return;
+
         isAttacking = true;
         attackTime = kickDuration;
         currentState = PlayerState.Attacking;
+
         animator.Play("PlayerKick", 0, 0f);
         kickHitbox.SetActive(true);
     }
 
-    // Optional special move
-    // private void Special() { ... }
+    private void Special()
+    {
+        // Reserved for later
+    }
+
+    private void StartDash(float direction)
+    {
+        isDashing = true;
+        dashTimeLeft = dashDuration;
+        lastDirection = direction;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.collider.CompareTag("Ground")) return;
+
+        isGrounded = true;
+        animator.SetBool("IsJumping", false);
+
+        if (currentState != PlayerState.Hit && currentState != PlayerState.Dead)
+            currentState = PlayerState.Idle;
+    }
 
     public void DisableHitboxes()
     {
         punchHitbox.SetActive(false);
         kickHitbox.SetActive(false);
-        // specialHitbox.SetActive(false);
     }
 
     public void EnterHitState(float duration)
     {
         currentState = PlayerState.Hit;
-        StartCoroutine(HitStateCoroutine(duration));
+        Invoke(nameof(ExitHitState), duration);
     }
 
-    private System.Collections.IEnumerator HitStateCoroutine(float duration)
+    private void ExitHitState()
     {
-        yield return new WaitForSeconds(duration);
-        if (currentState != PlayerState.Dead) currentState = PlayerState.Idle;
+        if (currentState != PlayerState.Dead)
+            currentState = PlayerState.Idle;
     }
 
     public void EnterDeadState()
@@ -426,4 +248,24 @@ public class PlayerMovement : MonoBehaviour
         currentState = PlayerState.Dead;
         rb.velocity = Vector2.zero;
     }
+
+    private void FaceOpponent()
+    {
+        if (!opponent) return;
+
+        if (transform.position.x < opponent.position.x)
+            transform.localScale = new Vector3(1, 1, 1);
+        else
+            transform.localScale = new Vector3(-1, 1, 1);
+    }
+    // private void FaceOpponent()
+    // {
+    //     if (!opponent || !spriteRenderer) return;
+
+    //     // Face right if opponent is on the right
+    //     bool opponentOnRight = opponent.position.x > transform.position.x;
+
+    //     // flipX = true means sprite faces LEFT
+    //     spriteRenderer.flipX = !opponentOnRight;
+    // }
 }
