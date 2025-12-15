e1cfb243debc850ce94bb04cd1a1042a20e1100f
praseth002 <prasethsokha@gmail.com>
2025-12-15 10:16:25 +0700

    please allah

--- Assets/Scripts/Player/TestDamage.cs
+++ Assets/Scripts/Player/TestDamage.cs
@@ -1,3 +1,18 @@
+// // using UnityEngine;
+
+// // public class DamageTester : MonoBehaviour
+// // {
+// //     public PlayerHealth playerHealth;
+
+// //     void Update()
+// //     {
+// //         if (Input.GetKeyDown(KeyCode.H)) // press H to simulate hit
+// //         {
+// //             playerHealth.TakeDamage(10, Vector2.left);
+// //         }
+// //     }
+// // }
+
 // using UnityEngine;
 
 // public class DamageTester : MonoBehaviour
@@ -8,23 +23,8 @@
 //     {
 //         if (Input.GetKeyDown(KeyCode.H)) // press H to simulate hit
 //         {
-//             playerHealth.TakeDamage(10, Vector2.left);
+//             Vector2 testDirection = Vector2.left; // push player left
+//             playerHealth.TakeDamage(10, testDirection);
 //         }
 //     }
 // }
-
-using UnityEngine;
-
-public class DamageTester : MonoBehaviour
-{
-    public PlayerHealth playerHealth;
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.H)) // press H to simulate hit
-        {
-            Vector2 testDirection = Vector2.left; // push player left
-            playerHealth.TakeDamage(10, testDirection);
-        }
-    }
-}
